76254af2a2319b7b829eb39f360b2a2c2b520021
iambikash-777 <110771484+iambikash-777@users.noreply.github.com>
2023-09-16 23:22:47 +0545

    Add files via upload

---
+++ DataStructures.py
@@ -0,0 +1,20 @@
+#Data Structures in python- the simple types
+x=1 # Integers(Whole Numbers)
+print(x)
+#FLOATING POINT NUMBERS (Real Numbers)
+x =1.0
+print('The number is', x)
+ #COMPLEX NUMBERS
+x=2+3j
+print(x)     
+#BOOLEAN NUMBERS
+x=True
+print(not x)
+#String Type (Characters or Texts)
+msg="Hi everyone! This is bikash"
+print(msg)
+#Special object indicating nulls
+a =None
+print(a) 
+ 
+
